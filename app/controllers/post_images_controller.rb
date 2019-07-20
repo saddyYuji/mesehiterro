b9cb2c0eca17267ee292329fda09afc16e40c937
saddyYuji <saddy.yuji@gmail.com>
2019-07-20 16:18:56 +0900

    [Finish] 5-3

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,10 +1,22 @@
 class PostImagesController < ApplicationController
 	def new #画像投稿画面を表示する
+		@post_image = PostImage.new
 	end
 	def create #投稿された画像を保存する
+		@post_image = PostImage.new(post_image_params)
+		@post_image.user_id = current_user.id
+		@post_image.save
+		redirect_to post_images_path
 	end
 	def index #投稿画像をリスト表示する画面を表示する
+		@post_images = PostImage.all
 	end
 	def show #投稿画像の詳細画面を表示する
+		@post_image = PostImage.find(params[:id])
+	end
+
+	private
+	def post_image_params
+		params.require(:post_image).permit(:shop_name, :image, :caption)
 	end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4b87853fac22187817613a4efee1cd9075699adb34bd71e500a20bb1e474cda75fcae1d3476a1d41486f40039de023c7e57708ec748538877ecf623b73d72c28'
